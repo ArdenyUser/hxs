3a8461ee3cd4c9e5f4f5e7cb0d1da3f8b7f7e540
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-03-09 18:17:30 -0500

    1

--- SETTUP.py
+++ SETTUP.py
@@ -1,1 +1,5 @@
-
+b = input("Enter the setting's, HXSP files usaully, directory that stores data: ")
+filename = b
+myfile = open(filename, 'w')
+myfile.write('Written with Python\n')
+myfile.close()
