1c5305a9a29127dac2885ae9b20db0f4d25fea4f
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-03-09 18:38:35 -0500

    in construction

--- compile.py
+++ compile.py
@@ -1,1 +1,19 @@
-
+f = open('locate_settings.hxsp')
+a = f
+f.close()
+filename = a + "hbdockread.hxsp"
+x = open(filename, 'r')
+location = x
+x.close()
+filename = a + "hbdockread.hxsp"
+myfile = open(filename, 'w')
+location = myfile
+myfile.close()
+hxst = open(location + "type.hxs")
+typef = hxst
+hxst.close()
+if typef == "DOC":
+  hxsm = open(location + "main_text.hxs")
+  text = hxsm
+  hxsm.close()
+  print(text)
